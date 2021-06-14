fcba3c957b0de6caa0b33a6ef1064af18baa9eab
Jorge Bueno Perez <jorgebuepe93@gmail.com>
2021-06-14 11:21:38 +0200

    Update Diebold-Mariano_test.R

--- Diebold-Mariano_test.R
+++ Diebold-Mariano_test.R
@@ -3,5 +3,4 @@ library(forecast)
 df <- read.csv("~/Desktop/Files/Thesis/Model/DF/df.csv")
 df
 
-dm.test(df$LSTM, df$Naive, alternative = "less", h = 1) # DM = -6.3632, Forecast horizon = 1, Loss function power = 2, p-value = 1.691e-10
-dm.test(df$Drift, df$LSTM, alternative = "less", h = 1) # DM = 7.9558, Forecast horizon = 1, Loss function power = 2, p-value = 1
+dm.test(df$Drift, df$LSTM, alternative = "less", h = 1)
